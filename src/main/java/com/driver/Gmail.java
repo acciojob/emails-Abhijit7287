dae3e892a134308aabf9fd9342a196b2fa0ecf2c
Abhijit_B <bandgarabhijitb@gamil.com>
2023-07-23 16:45:49 +0530

    new Commit

--- src/main/java/com/driver/Gmail.java
+++ src/main/java/com/driver/Gmail.java
@@ -109,3 +109,41 @@ public class Gmail extends Email {
         return this.inboxCapacity;
     }
 }
+
+class Mail {
+
+    Date date;
+    String sender;
+    String message;
+
+    public Mail(Date date, String sender, String message) {
+        this.date = date;
+        this.sender = sender;
+        this.message = message;
+    }
+
+    public Date getDate() {
+        return date;
+    }
+
+    public void setDate(Date date) {
+        this.date = date;
+    }
+
+    public String getSender() {
+        return sender;
+    }
+
+    public void setSender(String sender) {
+        this.sender = sender;
+    }
+
+    public String getMessage() {
+        return message;
+    }
+
+    public void setMessage(String message) {
+        this.message = message;
+    }
+}
+
